cb7d303f7dec026e0437d5a758d46fd5e3f53f3f
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-05-22 12:33:40 +0900

    memo-delete

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,6 +1,7 @@
 class MemosController < ApplicationController
 
   def index
+    @memos = Memo.all
   end
 
   def new
@@ -9,6 +10,33 @@ class MemosController < ApplicationController
 
   def create
     @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to memos_path
+    else
+      redirect_to new_memo_path
+    end
+  end
+
+  def edit
+    @memo = Memo.find(params[:id])
+  end
+
+  def update
+    @memo = Memo.find(params[:id])
+    if @memo.update(memo_params)
+      redirect_to memos_path
+    else
+      redirect_to edit_memos_path
+    end
+  end
+
+  def destroy
+    memo = Memo.find(params[:id])
+    if memo.destroy
+    redirect_to memos_path
+    else
+    redirect_to memos_path
+    end
   end
 
   private
